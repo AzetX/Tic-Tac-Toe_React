72d5ceb3f3c48517aaa8709e6ca235e1fa73ed9d
AzetX <llleogad@gmail.com>
2024-08-01 16:35:57 +0300

    master: Tic-Tac-Toe game

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Game from "./components/Game";
+import "./App.css";
+import React from "react";
+
+
+function App() {
+
+  return (
+    <React.StrictMode>
+       <Game />
+    </React.StrictMode>
+  )
+}
+
+export default App
